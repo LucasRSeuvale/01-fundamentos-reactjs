4bb2462400071bd322fff073a8bc465054e687cc
Lucas Rodrigues <lucas.rodrigues@seuvale.com>
2022-10-05 17:31:44 -0300

    Começo-curso

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import {Post} from './post';
+export function App() {
+
+  return (
+    <div>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  )
+}
+
+
