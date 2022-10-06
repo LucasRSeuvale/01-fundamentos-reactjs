233722ad0ccb838f9af4edfbf254557135c33df6
Lucas Rodrigues <lucas.rodrigues@seuvale.com>
2022-10-06 17:44:26 -0300

    start of the feed project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
+
 import {Post} from './post';
+
 export function App() {
 
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post 
+            author = "Eduardo Barros"
+            content = "testando react1"
+      />
+
+      <Post 
+          author = "lucas Rodrigues"
+          content = "testando react2"
+      />
     </div>
   )
 }
-
-
